93bdb0e632a4f83df93461851a39721ab080227d
bluecubesociety <alexander@bratrich-online.de>
2023-09-17 19:03:44 +0200

    v1.3

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
-# Youtube Sort
+# YouTube Sort
 
-A Firefox extension that sorts YouTube video tabs based on video duration.
+Enhance your YouTube viewing experience, by sorting your open video tabs by various stats (title, channel name, view count and more).
 
 ## Patch Notes
+
+### Version 1.3 - September 17, 2023
+
+**A New Look**
+- Changed the GUI design, to fit more into YouTube's design
+- Added a new tab to separate the settings from the rest
+- Video dectection indicator changed, and now recived a helper text, so you know what it is
+
+**New Features**
+- You can now sort by something else than just the duration! You can sort by title, duration, channel name, views, and upload date
+- You can now ignore live streams
+- These new sorting methods can be combined, so if you sort videos by channel name, and the order isn't obvious because there more than one video by that channel, you can then sort further (by something else)
+- Improved collection of video data (all these are collected from the videoObject-Schema instead of the DOM)
+- Now includes a list of detected tabs (mostly just for me for debugging).
+
+**Fixes and Improvements**
+- Videos get now saved using their Video ID, and not their URL, which means the same videos with different URLs don't get saved twice and get treated the same way. This also reduces some URL-regex code when trying to find the ID, and reduces valuable data length in the storage.
+- Videos get marked as live or playlist before they get saved. This does increase the storage a tiny bit, but simplifies filtering later
+- When filtering, old entries in the storage that can not be found anymore will not get deleted. In general, the filtering has been improved quite a bit, bit still could be improved further
+- Styling now uses SCSS (but hasn't been used to its full potential)
+- Improved code structure and naming.
+
+**Known Issues and Limits, and new Bugs**
+- Video Premieres that go something like "Premieres in X days" do not get tracked or sorted, because their videos don't have a duration (yet), and I don't track the time until the premiere (but if there's someone out there who has such a specific use case that they have multiple premieres open at once and want them sorted, let me know or [contribute](https://github.com/alexandertbratrich/youtube-sort) to the code).
+- For the moment, I removed statistics, but if you like them back, let me know.
+- Due to the new data collection via schema, shorts seem to be problem for that, because somehow their schema isn't know right away, and if it is, it doesn't include all information (usually just the uploader) and the schema is structured differently. Shorts are a bit of a problem in general, due to their nature of just scrolling further, so that would mean I would need to track if the url changes and update accordingly. I may update that in the future, but I personally don't watch shorts that much that I need to sort them, but if that's a special case you want to me to patch for you, let me know.
+- For unknown reason, sometimes videos doesn't get detected (or don't get saved into the storage), or get sometimes mislabelled as a live stream, even though they aren't.
+
+**Things I'd love to improve in the future**
+- System dark/light theme preferences
+- Better styling
+- Bring back shorts and stats
+- Maybe make list of tabs clickable
+- Icon on the store page (white on white)
+- Include playback speed when sorting by duration
 
 ### Version 1.2 - April 23, 2023
 
@@ -17,14 +52,21 @@ A Firefox extension that sorts YouTube video tabs based on video duration.
 - You can now choose the sorting order (ascending, descending)
 
 **Misc Changes**
-- Updated manifest, package and Firefox store meta data (because someone forgot to remove tutorial data)
-- Removed icon badge entirely (because it was causing more trouble maintaining than it was actually useful for).
+- Updated manifest, package and Firefox store metadata (because someone forgot to remove tutorial data)
+- Removed icon badge entirely (because it was causing more trouble maintaining, than it was actually useful for).
 
-**Known Issues**
+**Known Issues and Limits**
 - The extension currently always adds video duration data into the local storage, but never deletes it once the tab is closed.
+- Live-Streams get sorted randomly (should be either ignore entirely or get a better duration)
+- Ignoring Sleepy Tabs (not loading them) will still sort them if they are in the storage (which is a good thing, but should be up to the user)
+- Is it possible to change the YouTube icon?
+- Fix Icon in Store
+- Add a button to manually clear storage
+- Use video ideas in storage only (so if multiple windows/tabs have the same video open, but with a different URL (i.e. query parameters), they still use the same slot in the storage, saving space)
+- What is the storage limit?
 
 ### Initial Version - March 31, 2022
 
-- Marks open YouTube videos discretly when detected
+- Highlights the runtime in open YouTube videos when detected
 - Grabs the runtime of all loaded YouTube tabs and stores it in the local browser storage
 - Shows the total amount of open YouTube tabs as a badge
---
+++ src/main.js
@@ -0,0 +1,319 @@
+const regex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]*)(\S+)?/i
+
+const settings = {
+  ignore_inactive: false,
+  ignore_playlists: false,
+  ignore_live: false,
+  sorting: [
+    { dropdown: ["A-Z","Z-A"], asc: false, order: 0, attr: "title", title: "Video Title" },
+    { dropdown: ["Oldest first","Newest first"], asc: false, order: 1, attr: "uploadDate", title: "Upload Date" },
+    { dropdown: ["Least first","Most first"], asc: false, order: 2, attr: "views", title: "Views" },
+    { dropdown: ["A-Z","Z-A"], asc: false, order: 3, attr: "author", title: "Channel Name" },
+    { dropdown: ["Shortest first","Longest first"], asc: false, order: 4, attr: "duration", title: "Video Duration" },
+  ],
+  menu: 0
+}
+
+/** saves extension settings in the local storage */
+async function updateSettings () {
+  await browser.storage.sync.set({"settings": settings});
+}
+
+/** loads settings from the sync storage */
+async function getSettings () {
+  const { settings: loadedSettings } = await browser.storage.sync.get("settings");
+  const updatedSettings = { ...settings, ...loadedSettings };
+  Object.assign(settings, updatedSettings);
+}
+
+/** returns a list of tabs, the way the should be sorted. */
+async function prefilterTabs () {
+  const detectedTabs = await browser.storage.local.get()
+  const allTabs = await browser.tabs.query({
+    pinned: false,
+    url: "*://*.youtube.com/*"
+  })
+
+  // removes all info I don't care about and remaps all data
+  const trimmedAllTabs = allTabs.map(tab => {
+    const youtubeID = regex.exec(tab.url)[6]
+    return {
+      sleepy: tab.discarded,
+      selected: tab.highlighted,
+      id: tab.id,
+      ...(youtubeID ? { youtubeID: youtubeID } : {})
+    }
+  })
+  const storedTabs = Object.entries(detectedTabs).map(tab => {
+    return {
+      youtubeID: tab[0],
+      ...tab[1]
+    }
+  })
+
+  // the combinedArray includes all open window tabs and all detected entries.
+  // objects that have no id, are old entries that need to go
+  // objects that have no duration (or other), are tabs that have not beed detected
+  const mergedMap = new Map();
+  for (const tab of trimmedAllTabs.concat(storedTabs)) {
+    if (!mergedMap.has(tab.youtubeID)) {
+      mergedMap.set(tab.youtubeID, {});
+    }
+    Object.assign(mergedMap.get(tab.youtubeID), tab);
+  }
+  const combinedArray = Array.from(mergedMap.values()).filter(tab => {
+    return tab.youtubeID &&
+    (settings.ignore_playlists && !tab.playlist) &&
+    (settings.ignore_live && !tab.live) &&
+    (settings.ignore_inactive && !tab.sleepy)
+  });
+
+  // removes entries from storage that can not be found anymore
+  combinedArray.forEach(async tab => {
+    if (!tab.id && !tab.title) {
+      await browser.storage.local.remove(tab.youtubeID)
+    }
+  })
+
+  const selectedTabs = combinedArray.filter(tab => tab.selected)
+  
+  return selectedTabs.length > 1 ? selectedTabs : combinedArray
+}
+
+/** sorts tabs based on settings. */
+async function sortTabs () {
+  document.getElementById("tab-button-sort").classList.add("loading");
+  document.getElementById("alert-error").innerText = "";
+
+  try {
+    const tabs = await prefilterTabs();
+    const sortedTabs = tabs.sort((a,b) => {
+      for (const sorting of settings.sorting) {
+        const criteria = sorting.attr;
+        const critA = typeof a[criteria] === "string" ? a[criteria].toLowerCase() : a[criteria]
+        const critB = typeof b[criteria] === "string" ? b[criteria].toLowerCase() : b[criteria]
+        let res = String(critA).localeCompare(critB, undefined, { numeric: true });
+        if (sorting.asc === true && res !== 0) res = -res
+        if (res !== 0) return res
+      }
+    })
+
+    sortedTabs.forEach(async (tab) => {
+      browser.tabs.move(tab.id, {index: -1})
+    })
+    renderList();
+
+    // wake them up, if wanted
+    /*if (settings.ignore_inactive !== true) {
+      sleepyTabs.forEach(tab => {
+        browser.tabs.reload(tab.id)
+      })
+    }*/
+
+  } catch (error) {
+    console.log("[YT Sort]", error);
+    document.getElementById("alert-error").innerText = "Error: " + error;
+  } finally {
+    document.getElementById("tab-button-sort").classList.remove("loading")
+  }
+}
+
+/** returns views as a string */
+function getViews (views) {
+  const SI_SYMBOL = ["", "K", "M", "B", "T"];
+  const tier = Math.log10(Math.abs(views)) / 3 | 0;
+  if (tier === 0) return views.toString();
+  const divisor = Math.pow(10, tier * 3);
+  return (views / divisor).toFixed(1) + SI_SYMBOL[tier];
+}
+
+/** returns duration as a string */
+function getDuration (seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formattedHours = hours ? String(hours).padStart(2, '0') : "";
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  return `${formattedHours ? formattedHours+":" : ""}${formattedMinutes}:${formattedSeconds}`
+}
+
+/** renders the list of detected tabs. */
+async function renderList () {
+  const tabList = document.getElementById("video-list");
+  tabList.innerHTML = "";
+
+  const tabs = await prefilterTabs();
+  for (const tab of tabs) {
+    const el = document.createElement('div');
+    el.id = tab.youtubeID;
+    el.classList.add('item');
+    
+    const titleElement = document.createElement("p");
+    titleElement.className = "title";
+    titleElement.textContent = tab.title;
+    el.appendChild(titleElement);
+
+    const smallElement = document.createElement("small");
+    const properties = [
+      { prop: "live", text: "Live", className: "badge" },
+      { prop: "playlist", text: "Playlist", className: "badge" },
+      { prop: "duration", textFunc: getDuration },
+      { prop: "uploadDate", textFunc: (date) => new Date(date).toLocaleDateString() },
+      { prop: "views", textFunc: (views) => `${getViews(views)} Views` },
+      { prop: "author" }
+    ];
+    properties.forEach(({ prop, text, textFunc, className }) => {
+      if (tab[prop]) {
+        const spanElement = document.createElement("span");
+        if (className) spanElement.className = className;
+        spanElement.textContent = textFunc ? textFunc(tab[prop]) : tab[prop];
+        smallElement.appendChild(spanElement);
+      }
+    });
+    el.appendChild(smallElement);
+    tabList.appendChild(el);
+  }
+}
+
+
+/** changes the active menu in the settings and saves it */
+async function deleteStorage () {
+  await browser.storage.local.clear();
+}
+
+/** changes the active menu in the settings and saves it */
+async function setActiveMenu (menu) {
+  settings.menu = menu;
+  renderMenu();
+  await updateSettings();
+}
+
+/** hides the list menu and shows the settings menu. */
+function showSettings () {
+  document.getElementById("tab-list").classList.add("hidden");
+  document.getElementById("tab-settings").classList.remove("hidden");
+  document.getElementById("tab-button-list").classList.remove("active");
+  document.getElementById("tab-button-settings").classList.add("active");
+}
+
+/** hides the settings menu and shows the list menu. */
+function showList () {
+  document.getElementById("tab-settings").classList.add("hidden");
+  document.getElementById("tab-list").classList.remove("hidden");
+  document.getElementById("tab-button-settings").classList.remove("active");
+  document.getElementById("tab-button-list").classList.add("active");
+  renderList();
+}
+
+/** shows the correct menu, depending on the settings. */
+function renderMenu () {
+  if (settings.menu === 1) {
+    showList();
+  } else {
+    showSettings();
+  }
+}
+
+/** renders the sort options incl the buttons and dropdowns */
+function renderSortOptions() {
+  const container = document.getElementById("sortable-list");
+  container.innerHTML = "";
+
+  for (const sortRule of settings.sorting) {
+    const dropdown = document.createElement('select');
+    dropdown.addEventListener("click", (e) => changeSortOrder(sortRule.attr, undefined, e))
+
+    const option1 = document.createElement("option");
+    option1.value = "false";
+    if (sortRule.asc !== true) {
+      option1.selected = true;
+    }
+    option1.textContent = sortRule.dropdown[0];
+
+    const option2 = document.createElement("option");
+    option2.value = "true";
+    if (sortRule.asc === true) {
+      option2.selected = true;
+    }
+    option2.textContent = sortRule.dropdown[1];
+
+    dropdown.appendChild(option1);
+    dropdown.appendChild(option2);
+
+    const buttonUp = document.createElement('button');
+    buttonUp.classList.add("up")
+    buttonUp.addEventListener("click", () => changeSortOrder(sortRule.attr, false))
+    buttonUp.innerHTML = '<svg height="24" viewBox="0 0 24 24" width="24" transform="scale(1, -1)"><path d="m18 9.28-6.35 6.35-6.37-6.35.72-.71 5.64 5.65 5.65-5.65z"></path></svg>'
+
+    const buttonDown = document.createElement('button');
+    buttonDown.classList.add("down")
+    buttonDown.addEventListener("click", () => changeSortOrder(sortRule.attr, true))
+    buttonDown.innerHTML = '<svg height="24" viewBox="0 0 24 24" width="24"><path d="m18 9.28-6.35 6.35-6.37-6.35.72-.71 5.64 5.65 5.65-5.65z"></path></svg>'
+
+    const buttons = document.createElement('div');
+    buttons.classList.add("buttons")
+    buttons.appendChild(dropdown)
+    buttons.appendChild(buttonUp)
+    buttons.appendChild(buttonDown)
+    
+    const el = document.createElement('li');
+    el.id = sortRule.attr;
+    el.classList.add('item');
+    const spanElement = document.createElement("span");
+    spanElement.className = "title";
+    spanElement.textContent = sortRule.title;
+    el.appendChild(spanElement);
+
+    el.appendChild(buttons)
+    container.appendChild(el);
+  }
+}
+
+async function changeSortOrder (attr, down, e) {
+  const index = settings.sorting.findIndex(item => item.attr === attr);
+  if (down === true) [settings.sorting[index], settings.sorting[index + 1]] = [settings.sorting[index + 1], settings.sorting[index]];
+  else if(down === false) [settings.sorting[index], settings.sorting[index - 1]] = [settings.sorting[index - 1], settings.sorting[index]];
+  if (e !== undefined) settings.sorting[index].asc = JSON.parse(e.target.value)
+  renderSortOptions();
+  await updateSettings();
+}
+
+async function renderSettings() {
+  document.getElementById("ignore-inactive").checked = settings.ignore_inactive
+  document.getElementById("ignore-live").checked = settings.ignore_live
+  document.getElementById("ignore-playlists").checked = settings.ignore_playlists
+}
+
+async function changeSetting(setting, e) {
+  settings[setting] = e.target.checked === true ? true : false;
+  renderSettings();
+  await updateSettings();
+}
+
+async function init () {
+  await getSettings();
+
+  // show the correct menu tab
+  document.getElementById("tab-button-settings").addEventListener("click", () => setActiveMenu(0));
+  document.getElementById("tab-button-list").addEventListener("click", () => setActiveMenu(1));
+  renderMenu();
+
+  // update version
+  document.getElementById("version-number").innerText = browser.runtime.getManifest().version;
+  
+  // render sort options and other settings
+  renderSortOptions();
+  await renderSettings();
+
+  // set events
+  document.getElementById("ignore-inactive").addEventListener("click", (e) => changeSetting("ignore_inactive", e));
+  document.getElementById("ignore-live").addEventListener("click", (e) => changeSetting("ignore_live", e));
+  document.getElementById("ignore-playlists").addEventListener("click", (e) => changeSetting("ignore_playlists", e));
+  
+  document.getElementById("delete-storage").addEventListener("click", deleteStorage);
+  document.getElementById('tab-button-sort').addEventListener("click", sortTabs);
+}
+init();
