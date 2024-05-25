a8e74c600b91b2ffba038dcd769b7fa24fd50a53
alexandertbratrich <alexandertbratrich@pm.me>
2024-05-25 19:45:37 +0200

    updated web-ext-config to ignore af-files, scss-files and the store-icon

--- web-ext-config.js
+++ web-ext-config.js
@@ -2,5 +2,11 @@ module.exports = {
     ignoreFiles: [
         'package-lock.json',
         'PUBLISH.MD',
+        'icons/*.afdesign',
+        'icons/*.afphoto',
+        'icons/store-icon.png',
+        'icons/*.svg',
+        'styles/*.scss',
+        'styles/*.map'
     ],
 };
